73458a57db37c14adb5f640063c58c5075c5ff72
Luis Galicia <david_galicia_garcia@outlook.es>
2019-12-10 14:30:20 -0600

    Actualización de celular
    Se creo el método para verificar por mensaje

--- ServiciosWebProInve/WebServicesProInve/src/java/pojos/VerificacionCorreo.java
+++ ServiciosWebProInve/WebServicesProInve/src/java/pojos/VerificacionCorreo.java
@@ -23,41 +23,43 @@ import javax.mail.internet.MimeMessage;
  */
 public class VerificacionCorreo {
 
-    private String remitente = "wacha_out@outlook.es";  //Para la dirección nomcuenta@gmail.com
+    private String remitente = "lugadaga_skater@hotmail.com";  //Para la dirección nomcuenta@gmail.com
     private String clave = "Inquisidorduy97";
-    private String destinatario = "david_galicia_garcia@outlook.es";
+    private String destinatario = "jesus_hdezmtz@hotmail.com";
 
     public void verificando() throws MessagingException {
         int valorEntero = (int) Math.floor(Math.random() * (9999 - 1000 + 1) + 1000);
         System.out.println("El número de verificación es: " + valorEntero);
         Properties props = new Properties();
-        props.put("mail.smtp.host", "smtp.gmail.com");  //El servidor SMTP de Google
-        props.setProperty("mail.smtp.user", remitente);
-        props.setProperty("mail.smtp.clave", clave);    //La clave de la cuenta
-        props.setProperty("mail.smtp.auth", "true");    //Usar autenticación mediante usuario y clave
-        props.setProperty("mail.smtp.starttls.enable", "true"); //Para conectar de manera segura al servidor SMTP
-        props.setProperty("mail.smtp.port", "587"); //El puerto SMTP seguro de Google
+             props.setProperty("mail.transport.protocol", "smtp");
+        props.setProperty("mail.host", "smtp.live.com");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
 
-        Session session = Session.getDefaultInstance(props,  new Authenticator() {
- 
-                    protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication(
-                                "wacha_out@outlook.es", "Inquisidorduy97");
-                    }
-                });
-        MimeMessage message = new MimeMessage(session);
+
+        Session session = Session.getDefaultInstance(props);
+        session.setDebug(true);
 
         try {
+            MimeMessage message = new MimeMessage(session);
+
             message.setFrom(new InternetAddress(remitente));
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(destinatario));   //Se podrían añadir varios de la misma manera
-            message.setSubject("Verificación de correo");
-            message.setText("Tu número de verificación de correo es: " + valorEntero);
-            Transport transport = session.getTransport("smtp");
-            transport.connect("wacha_out@outlook.es", "Inquisidorduy97");
-            transport.sendMessage(message, message.getAllRecipients());
-            transport.close();
-        } catch (MessagingException me) {
-            me.printStackTrace();   //Si se produce un error
+
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(destinatario));
+
+            message.setSubject("Solicitud de mensaje");
+
+            message.setText("Tu código de verificación es: " + valorEntero);
+
+            Transport trans = session.getTransport("smtp");
+            trans.connect("smtp.live.com", 587, remitente, clave);
+            trans.sendMessage(message, message.getAllRecipients());
+
+            System.out.println("Message Sent!");
+
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
         }
 
     }
--- ServiciosWebProInve/WebServicesProInve/src/java/pojos/VerificacionTelefono.java
+++ ServiciosWebProInve/WebServicesProInve/src/java/pojos/VerificacionTelefono.java
@@ -19,17 +19,15 @@ public class VerificacionTelefono {
   public static final String ACCOUNT_SID = "AC6470e628cdfe127b72a9ebfe5c544647";
   public static final String AUTH_TOKEN = "cf1ff6bf4b020962e8288477e7832c69";
 
- /* public static void main(String[] args) throws MessagingException {
+  public static void main(String[] args) throws MessagingException {
     Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
-    VerificacionCorreo v = new VerificacionCorreo();
-    v.verificando();
     int valorEntero = (int) Math.floor(Math.random()*(9999-1000+1)+1000);
     Message message = Message.creator(new PhoneNumber("+522281818876"),
         new PhoneNumber("+18563241001"), 
         "Que tranza perro, dame el número de verificación" + valorEntero).create();
-      System.out.println("El número es: " + valorEntero);
-    
-  }*/
+        System.out.println(message.getSid());
+        System.out.println("El número es: " + valorEntero);
+  }
 
 }
